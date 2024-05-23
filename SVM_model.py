989d8d8789698c8c5b2f8d4f15b8cd1450f5201b
Madelyn Nguyen <madelynguyen@gmail.com>
2024-05-23 09:25:45 -0700

    fixed X and y

--- SVM_model.py
+++ SVM_model.py
@@ -1,21 +1,33 @@
 ### SVM MODEL ###
 
+'''
+refered to: https://scikit-learn.org/stable/modules/svm.html
+'''
+
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn import svm  # library for SVM
+from sklearn.model_selection import train_test_split  #library to split test/train data
+from sklearn import svm  #library for SVM
+
 
 # load dataset
 df = pd.read_csv('healthcare-dataset-stroke-data.csv')
 
+# drop 'id' column
+df = df.drop(columns=['id'])
+
 #display head of csv file (just to visualize)
 print(df.head())
 
-# define X (data) and y (target)  #FIXME: not working rn bc data is in df
-X = df.data
-y = df.target
+# define X (data) and y (target)
+X = df.drop(columns=['stroke'])  #input data = all features EXCEPT STROKE
+y = df['stroke']  #target variable = stroke 
+
+#split into training and test data 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 #intializing model
 model = svm.SVC()
