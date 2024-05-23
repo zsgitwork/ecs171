5f6a9dd04e9cddb46cb29ebb4bbe7187b35f7182
Madelyn Nguyen <madelynguyen@gmail.com>
2024-05-23 08:26:07 -0700

    FIXME: defining X and y not finished!

--- model.py
+++ model.py
@@ -10,8 +10,16 @@ from sklearn import svm  # library for SVM
 # load dataset
 df = pd.read_csv('healthcare-dataset-stroke-data.csv')
 
+#display head of csv file (just to visualize)
+print(df.head())
+
+# define X (data) and y (target)  #FIXME: not working rn bc data is in df
+X = df.data
+y = df.target
+
 #intializing model
 model = svm.SVC()
+
 
 
 '''
