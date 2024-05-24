766a56bdbe4dbbd14accf2bde3f28ce3d144f0a2
Madelyn Nguyen <madelynguyen@gmail.com>
2024-05-23 19:09:39 -0700

    FIXME: need to check accuracy, etc

--- SVM_model.py
+++ SVM_model.py
@@ -1,7 +1,10 @@
 ### SVM MODEL ###
 
+# Challenge: too many variables, SVM best for comparing 2 specific features
+
 '''
 refered to: https://scikit-learn.org/stable/modules/svm.html
+and https://www.geeksforgeeks.org/support-vector-machine-algorithm/
 '''
 
 import pandas as pd
@@ -22,6 +25,16 @@ df = df.drop(columns=['id'])
 #display head of csv file (just to visualize)
 print(df.head())
 
+#fixing categories w/ non-number entries
+categories = ['gender', 'ever_married', 'work_type', 'Residence_type', 'smoking_status']
+df = pd.get_dummies(df, columns=categories)
+
+# Fill missing vals of bmi with mean
+df['bmi'] = df['bmi'].fillna(df['bmi'].mean())
+
+#display head of csv file (just to visualize)
+print(df.head())
+
 # define X (data) and y (target)
 X = df.drop(columns=['stroke'])  #input data = all features EXCEPT STROKE
 y = df['stroke']  #target variable = stroke 
@@ -31,3 +44,9 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 #intializing model
 model = svm.SVC()
+
+#fit model with training data
+model.fit(X_train, y_train)
+
+#make predictions with test data
+y_pred = model.predict(X_test)
